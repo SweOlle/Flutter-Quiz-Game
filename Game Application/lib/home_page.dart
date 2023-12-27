6961fc8089cfb87b8429073909a921c2379f5c20
SweOlle <80258629+SweOlle@users.noreply.github.com>
2023-12-27 22:48:58 +0100

    Improved home page

--- Game Application/lib/home_page.dart
+++ Game Application/lib/home_page.dart
@@ -60,7 +60,7 @@ class _HomePageState extends State<HomePage> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => TruthorDarePage()),
+                          builder: (context) => TruthOrDarePage()),
                     );
                   },
                 ),
