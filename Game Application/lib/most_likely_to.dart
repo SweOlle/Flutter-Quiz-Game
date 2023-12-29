a29245f8f98bc6b776e040363070bf5ccf0f6fee
SweOlle <80258629+SweOlle@users.noreply.github.com>
2023-12-29 15:12:00 +0100

    Added New game 'Most Likely to'

---
+++ Game Application/lib/most_likely_to.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'most_likely_to_data.dart';
+
+class MostLikelyToPage extends StatefulWidget {
+  @override
+  _MostLikelyToPageState createState() => _MostLikelyToPageState();
+}
+
+class _MostLikelyToPageState extends State<MostLikelyToPage> {
+  String _currentSentence = 'Tap to start';
+  int _points = 0;
+  bool _showGenerateButton = true;
+  bool _showGameButtons = false;
+  bool _mounted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _mounted = true;
+  }
+
+  @override
+  void dispose() {
+    _mounted = false;
+    super.dispose();
+  }
+
+  void _generateSentence() {
+    setState(() {
+      _currentSentence = _getRandomSentence();
+      _showGenerateButton = false;
+      _showGameButtons = true;
+    });
+  }
+
+  void _nextSentence() {
+    setState(() {
+      _currentSentence = _getRandomSentence();
+    });
+  }
+
+  String _getRandomSentence() {
+    // Replace this with your logic to fetch a random word
+    final randomIndex = Random().nextInt(mostLikelyToListEng.length);
+    return mostLikelyToListEng[randomIndex];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Most Likely To'),
+        backgroundColor: Colors.deepPurple[300],
+      ),
+      backgroundColor: Colors.deepPurple[300],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                '$_currentSentence',
+                style: TextStyle(fontSize: 46.0),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          _showGenerateButton
+              ? Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _generateSentence();
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text(
+                        'Start',
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                  ),
+                )
+              : SizedBox(), // Only show the button if _showGenerateButton is true
+          _showGameButtons
+              ? Column(
+                  children: [
+                    SizedBox(height: 20.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            _nextSentence();
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Text(
+                              'Next',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              : SizedBox(),
+          SizedBox(height: 20.0),
+        ],
+      ),
+    );
+  }
+}
---
+++ Game Application/lib/most_likely_to_data.dart
@@ -0,0 +1,25 @@
+List<String> mostLikelyToListEng = [
+  'Most likely to forget where they parked their car?',
+  'Most likely to become a famous actor/actress?',
+  "Most likely to win a Nobel Prize?",
+  "Most likely to spend all their money on something frivolous?",
+  "Most likely to become a world traveler?",
+  "Most likely to forget someone's birthday?",
+  "Most likely to become a famous musician?",
+  "Most likely to become a president or prime minister?",
+  "Most likely to be always late?",
+  "Most likely to win a cooking competition?",
+];
+
+List<String> mostLikelyToListSwe = [
+  "Mest sannolikt att glömma var de parkerade sin bil?",
+  "Mest sannolikt att bli en känd skådespelare/skådespelerska?",
+  "Mest sannolikt att vinna ett Nobelpris?",
+  "Mest sannolikt att spendera alla sina pengar på något frivolous?",
+  "Mest sannolikt att bli en världsresenär?",
+  "Mest sannolikt att glömma någons födelsedag?",
+  "Mest sannolikt att bli en känd musiker?",
+  "Mest sannolikt att bli president eller premiärminister?",
+  "Mest sannolikt att alltid vara sen?",
+  "Mest sannolikt att vinna en matlagningstävling?",
+];
